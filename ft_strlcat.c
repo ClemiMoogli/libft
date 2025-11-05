3c3024d7781b0039657a2774c8382d92f590382d
Clement <clementjeannin90@gmail.com>
2025-11-05 22:54:08 +0100

    enfin strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cjeannin <cjeannin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: clement <clement@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 13:36:12 by cjeannin          #+#    #+#             */
-/*   Updated: 2025/11/05 15:34:53 by cjeannin         ###   ########.fr       */
+/*   Updated: 2025/11/05 22:53:16 by clement          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,32 +18,36 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	len_dst;
 	size_t	len_src;
-	size_t	i;
+	size_t	len_dst_initial;
 	size_t	j;
 
-	j = 0;
-	i = 0;
 	len_dst = ft_strlen(dst);
 	len_src = ft_strlen(src);
-	if (size > (len_dst + len_src))
+	len_dst_initial = len_dst;
+	j = 0;
+	if (size == 0 || size <= len_dst)
+		return (size + len_src);
+	else if (len_dst < size && size <= (len_dst + len_src))
 	{
-		i = len_dst;
-		while (src[j])
+		while ((size - len_dst - 1)>0 && src[j])
 		{
-			dst[i] = src[j];
-			i++;
+			dst[len_dst] = src[j];
+			len_dst++;
 			j++;
 		}
-		dst[i] = '\0';
-		return (size);
+		dst[len_dst] = '\0';
 	}
-	while (len_dst < size - 1)
+	else if (size > (len_dst + len_src))
 	{
-		dst[len_dst] = src[i];
-		i++;
-		len_dst++;
+		while (src[j])
+		{
+			dst[len_dst] = src[j];
+			j++;
+			len_dst++;
+		}
+		dst[len_dst] = '\0';
 	}
-	return (len_dst + len_src);
+	return (len_dst_initial + len_src);
 }
 
 /*
@@ -65,4 +69,13 @@ int	main(){
 	printf("Vrai: %s\n", dest2);
 	return (0);
 }
+
+
+// concatener src a la fin de dest sans jamais depasser la taille total size du buffer dest.
+// on copie le plus possible de src dans dest sans depasser size et on retourne la longueur qu'on aurait eu si on avait assez de place
+//cas1: pas de place du tout (size=0)
+//cas2: size <= len_dest, pas assez de place pour faire quelque chose. on retourn le taille qu'on aurait eu.
+//cas3: len_dst < size <= len_dst + len_src, copier jusqu'a size-1 + '\0' et return len total
+//cas4: size > len_dst + len_src 
+
 */
