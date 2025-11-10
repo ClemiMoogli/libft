0dd9bb1c9928a373a330fd8963e1c2d30c60fd3a
Clement Jeannin <cjeannin@k1r1p1.42mulhouse.fr>
2025-11-10 12:43:26 +0100

    v6 clean mais il faut corriger les bonus

--- ft_lstsize_bonus.c
+++ ft_lstsize_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*   ft_lstsize_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cjeannin <cjeannin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/07 14:46:24 by clement           #+#    #+#             */
-/*   Updated: 2025/11/10 08:53:34 by cjeannin         ###   ########.fr       */
+/*   Updated: 2025/11/10 12:43:02 by cjeannin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,3 +26,13 @@ int	ft_lstsize(t_list *lst)
 	}
 	return (i);
 }
+/*
+int main(void)
+{
+	t_list	l;
+
+	l.content = "Ceci est un test";
+	l.next = "";
+	ft_lstsize(l);
+	return (0);
+}*/
